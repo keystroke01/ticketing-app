0b93b67fdd715b88292acf3deca4c7023e93761e
keystroke01 <80722661+keystroke01@users.noreply.github.com>
2021-12-21 03:31:19 +0400

    Added QTableWidget and sqlite3

--- TicketingApp.py
+++ TicketingApp.py
@@ -8,11 +8,17 @@ Created on Fri Dec 17 14:50:14 2021
 
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
+from PyQt5 import QtCore as qtc
+
+import sqlite3
+
+# Open the database and connect to it
+conn = sqlite3.connect("D:\Temp\data.db")
+c = conn.cursor()
 
 class NewTicket(qtw.QWidget):
     def __init__(self):
         super().__init__()
-        # self.setWindowTitle("Ticketing App")
         self.setObjectName("New Ticket")
         formLayout = qtw.QFormLayout()
         self.setLayout(formLayout)
@@ -34,8 +40,18 @@ class NewTicket(qtw.QWidget):
         self.ticketSeverity.addItems(['Minor', 'Major'])
         self.ticketStatus = qtw.QComboBox()
         self.ticketStatus.addItems(['Open', 'In Progress', 'Closed'])
-        self.submitButton = qtw.QPushButton("Submit")
+        self.submitButton = qtw.QPushButton("Submit",
+                                            clicked = lambda: self.addNewTicket())
         
+        
+        self.outputTable = qtw.QTableWidget(self) 
+        self.outputTable.setRowCount(1)
+        self.outputTable.setColumnCount(2)
+        self.outputTable.setHorizontalHeaderLabels(['Title', 'Desc'])
+        self.outputTable.setEditTriggers(qtw.QTableWidget.NoEditTriggers)
+        
+        
+    
         formLayout.addRow("Title", self.ticketTitle)
         formLayout.addRow("Description", self.ticketDesc)
         formLayout.addRow("Reported Date", self.reportedDate)
@@ -44,14 +60,28 @@ class NewTicket(qtw.QWidget):
         formLayout.addRow("Severity", self.ticketSeverity)
         formLayout.addRow("Status", self.ticketStatus)
         formLayout.addRow(self.submitButton)
+        formLayout.addRow(self.outputTable)
         # self.show()
+    
+    # Method to add a new ticket to the database    
+    def addNewTicket(self):
+        # sqlQuery = "INSERT INTO TICKETS (TITLE, DESCRIPTION) VALUES ("
+        # sqlQuery += f"'{self.ticketTitle.text()}', '{self.ticketDesc.toPlainText()}')"
+        # c.execute(sqlQuery)
+        print("Row added")
+        self.outputTable.setItem(0,0,qtw.QTableWidgetItem(self.ticketTitle.text()))
+        self.outputTable.setItem(0,1,qtw.QTableWidgetItem(self.ticketDesc.toPlainText()))
+        # self.outputTable.resizeColumnsToContents()
+        # self.outputTable.resizeRowsToContents()
+        self.outputTable.setWordWrap(True)
         
 
 class MainWindow(qtw.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Ticketing App")
-        
+        self.setBaseSize(qtc.QSize(1000,500))
+        self.setMinimumSize(qtc.QSize(1000,500))
         #Tab Widget is the central widget
         self.tabWidget = qtw.QTabWidget()
         self.tabWidget.setObjectName("tabWidget")
@@ -65,9 +95,13 @@ class MainWindow(qtw.QMainWindow):
         
         self.show()
         
-app = qtw.QApplication([])
-mainWindow = MainWindow()
-# mainWindow.show()
-app.exec_()
-
-#TESTING
+if __name__== "__main__":
+    # Start the app
+    app = qtw.QApplication([])
+    mainWindow = MainWindow()
+    app.exec_()
+    
+# Commit and close the connection
+    
+conn.commit()
+conn.close()
